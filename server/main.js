4ad5afebb575054b73eedfa0b284203b88bbdded
rebiss <barsegyanat.93@gmail.com>
2021-01-19 00:46:12 +0400

    add: gql

--- server/main.js
+++ server/main.js
@@ -1,11 +1,26 @@
 const express = require('express')
 const cors = require('cors')
 const {graphqlHTTP} = require('express-graphql')
+const schema = require('./schema/schema.gql.js')
+const users = [{id: 1, username: "Valodik", age: 28}]
 
 const app = express()
 app.use(cors())
-app.use('/graphql', graphqlHTTP({
-    graphql: true,
+
+const root = {
+    getAllUsers: () => {
+        return users
+    },
+    getUser: ({id}) => {
+        users.find(user => user.id == id)
+    }
+}
+
+
+app.use('/gq', graphqlHTTP({
+    graphiql: true,
+    schema,
+    rootValue: root,
 }))
 
 const port = process.env.PORT || 3011
